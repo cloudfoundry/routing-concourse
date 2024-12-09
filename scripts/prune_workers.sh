05eaf23fbad480880250a8817d1242b93d25b263
M Rizwan Shaik <m.rizwan.shaik@sap.com>
2024-12-09 12:13:51 +0100

    address review suggestions

--- scripts/prune_workers.sh
+++ scripts/prune_workers.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
+CONCOURSE_PASSWORD=$(grep CONCOURSE_ADMIN_PWD /concourse/.concourse.env | cut -d = -f 2)
 # Log in to Concourse
-fly -t networking-extensions login -c https://concourse.arp.cloudfoundry.org/ -u admin_concourse -p `grep CONCOURSE_ADMIN_PWD /concourse/.concourse.env | cut -d = -f 2`
+fly -t networking-extensions login -c https://concourse.arp.cloudfoundry.org/ -u admin_concourse -p "$CONCOURSE_PASSWORD"
 
 # Prune workers
 fly -t networking-extensions prune-worker --all-stalled
